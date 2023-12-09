b29f52fbae86fff21977e3a79eafad128e46db64
muslimumarov <norbekumarov613@gmail.com>
2023-12-09 12:37:07 +0500

    https://github.com/muslimumarov/reactno_1.github.io.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, {useEffect, useState } from 'react'
+export default function App() {
+  const [number, setNumber]=useState(1)
+  const [text, setText]=useState("")
+  useEffect(()=>{
+   setNumber(0)
+  }, [])
+  const addNumber=()=>{
+    setNumber(number+1)
+    setText("qo'shish")
+  }
+  const removeNumber=()=>{
+    setNumber(number-1)
+    setText("ayirish")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className="box">
+        <p className='text'>{text}</p>
+        <h1 className='title'>{number}</h1>
+        <div className="innerWrapper">
+          <button className='btn' onClick={removeNumber}>remove</button>
+          <button className='btn active' onClick={addNumber}>add</button>
+        </div>
+      </div>
     </div>
-  );
-}
 
-export default App;
+  )
+}
+// import React, { useEffect, useState } from 'react'
+// import "./App.css"
+// export default function App() {
+//   const [number, setNumber]=useState(4)
+//   const [text, setText]=useState("")
+//   useEffect(()=>{
+//     setNumber(0)
+//   }, [])
+//   const addNumber=()=>{
+//     setNumber(number+1)
+//     setText("qo'shish")
+//   }
+//   const removeNumber=()=>{
+//     setNumber(number-1)
+//     setText('ayirish')
+//   }
+
+
+//   return(
+//     <div className='box'>
+//    <div className="wrapper">
+//      <h1>{number}</h1>
+//      <button onClick={addNumber}>add</button>
+//      <button onClick={removeNumber}>remove</button>
+//      <p>{text}</p>
+//    </div>
+//     </div>
+//   )
+// }
